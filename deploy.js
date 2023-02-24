597294369349055e28fe34dad868b9a9db8a3d86
sheng yeong <chia.shengyeong@gmail.com>
2023-02-24 09:28:00 +0800

    add back require

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
- 
 const { abi, evm } = require('./compile');
+require('dotenv').config()
  
 provider = new HDWalletProvider(
   process.env.ACC_MNEMONIC,
